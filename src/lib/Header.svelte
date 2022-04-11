304a269343cd3ef133ce4b47c0b29dd08929cf0c
shawncocklin <shawncocklin@gmail.com>
2022-04-11 16:31:29 -0400

    some color changes and link styling

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,7 +1,3 @@
-<!-- <script>
-	import '../app.css'
-</script> -->
-
 <header>
 	<h1>Sick Beats</h1>
 	<nav>
@@ -22,5 +18,33 @@
 	h1 {
 		font-family: 'Raleway', sans-serif;
 		font-weight: 800;
+		font-size: 5rem;
+		line-height: 1;
+		margin-bottom: 0.5em;
+	}
+
+	a {
+		position: relative;
+	}
+
+	a:hover:after,
+	a:focus:after {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		height: 1px;
+		background-color: #e4e4e4;
+		animation: underline 150ms linear;
+	}
+
+	@keyframes underline {
+		0% {
+			width: 0%;
+		}
+		100% {
+			width: 100%;
+		}
 	}
 </style>
