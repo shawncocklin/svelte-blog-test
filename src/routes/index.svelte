c3eb51d8ca3cccf0f193b1e0a1c5e48c47ed929c
shawncocklin <shawncocklin@gmail.com>
2022-04-12 17:41:22 -0400

    preload landing page image before page render

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,31 @@
+<script>
+	function preload(src) {
+		return new Promise((res) => {
+			let img = new Image()
+			img.onload = res
+			img.src = src
+		})
+	}
+
+	let src = 'https://source.unsplash.com/random/400x300?music'
+</script>
+
 <!-- TODO: adjust hero copy -->
 
-<div class="flow">
-	<h2>Welcome to Sick Beats</h2>
-	<p>
-		Join me as I rate, review, and overanalyze the songs on my <a
-			class="underline"
-			href="https://open.spotify.com/playlist/0fyjsVZmoLbRrDI8CrBi2s?si=75a5fbff793a4938"
-			>Spotify playlist</a
-		>
-	</p>
+{#await preload(src) then}
+	<div class="flow">
+		<h2>Welcome to Sick Beats</h2>
+		<p>
+			Join me as I rate, review, and overanalyze the songs on my <a
+				class="underline"
+				href="https://open.spotify.com/playlist/0fyjsVZmoLbRrDI8CrBi2s?si=75a5fbff793a4938"
+				>Spotify playlist</a
+			>
+		</p>
 
-	<img src="https://source.unsplash.com/random/400x300?music" alt="" />
-</div>
+		<img {src} alt="" />
+	</div>
+{/await}
 
 <style>
 	div {
